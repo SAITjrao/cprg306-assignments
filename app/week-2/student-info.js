e73dae7d6d77bca753bd19af40476924c51641f7
SAITjrao <jazib.rao@edu.sait.ca>
2025-01-28 22:14:17 -0700

    edit root page to add links to week2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
     return(
         <main>
             <p>Jazib Rao</p>
-            <Link href="github.com/SAITjrao/cprg306-assignments">My Github Repository</Link>
+            <Link href="https://github.com/SAITjrao/cprg306-assignments">My Github Repository</Link>
         </main>
     )
 }
