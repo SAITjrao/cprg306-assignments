f8da7c848b9dc7370a28aaeebe8d2635b1f8afd5
SAITjrao <jazib.rao@edu.sait.ca>
2025-01-28 21:56:56 -0700

    add week-2 student-info component and page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <p>Jazib Rao</p>
+            <Link href="github.com/SAITjrao/cprg306-assignments">My Github Repository</Link>
+        </main>
+    )
+}
