90b89cc2c98a1affb39bf10be774c92dcd760756
SAITjrao <jazib.rao@edu.sait.ca>
2025-02-05 22:42:33 -0700

    add tailwind css styling to itemlist

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,8 @@
 export default function Item(props){
     return(
-        <li>
-            {props.name} <br/> Buy {props.quantity} in {props.category}
+        <li className="ml-2 mb-3 p-2 bg-indigo-900 bg-opacity-30 w-96">
+            <p className="text-lg font-semibold">{props.name}</p>
+            Buy {props.quantity} in {props.category}
         </li>
     )
 }
