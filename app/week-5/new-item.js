135aa451253ec5593640ce91d36cd16eaa3752d1
SAITjrao <jazib.rao@edu.sait.ca>
2025-02-08 20:51:39 -0700

    create week 5 structure

---
+++ app/week-5/new-item.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+
+export default function NewItem(){
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        if(quantity+1 < 21 ){
+            setQuantity(quantity + 1);
+        };
+    };
+
+    const decrement = () => {
+        if(quantity-1 > 0){
+            setQuantity(quantity - 1);
+        };
+    };
+
+    return (
+        <div className="m-6 w-16 bg-white">
+            <p className="p-3 text-black text-2xl text-center font-bold">{quantity}</p>
+            <button className="font-bold text-lg w-12 m-2 rounded bg-green-500 disabled:bg-gray-300" disabled={quantity === 20 ? true : false} onClick={increment}>+</button>    
+            <button className="font-bold text-lg w-12 m-2 rounded bg-red-500 disabled:bg-gray-300" disabled={quantity === 1 ? true : false} onClick={decrement}>-</button>    
+        </div>
+    )
+}
