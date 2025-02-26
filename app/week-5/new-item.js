bb32c576484b038550bebaccd5141e1307e53f64
SAITjrao <166628742+SAITjrao@users.noreply.github.com>
2025-02-25 22:00:15 -0700

    make buttons side-by-side

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -19,10 +19,10 @@ export default function NewItem(){
     };
 
     return (
-        <div className="m-6 w-16 bg-white">
+        <div className="m-6 w-30 bg-white flex">
             <p className="p-3 text-black text-2xl text-center font-bold">{quantity}</p>
-            <button className="font-bold text-lg w-12 m-2 rounded bg-green-500 disabled:bg-gray-300" disabled={quantity === 20 ? true : false} onClick={increment}>+</button>    
             <button className="font-bold text-lg w-12 m-2 rounded bg-red-500 disabled:bg-gray-300" disabled={quantity === 1 ? true : false} onClick={decrement}>-</button>    
+            <button className="font-bold text-lg w-12 m-2 rounded bg-green-500 disabled:bg-gray-300" disabled={quantity === 20 ? true : false} onClick={increment}>+</button>    
         </div>
     )
 }
