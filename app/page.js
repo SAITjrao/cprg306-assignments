11e894513e79740c886464cae08f4eb7c2356558
SAITjrao <jazib.rao@edu.sait.ca>
2025-03-05 18:12:14 -0700

    updating styling of homepage

--- app/page.js
+++ app/page.js
@@ -3,12 +3,15 @@ import Link from "next/link"
 export default function Home(){
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="week-2">Week 2</Link><br />
-      <Link href="week-3">Week 3</Link><br />
-      <Link href="week-4">Week 4</Link><br />
-      <Link href="week-5">Week 5</Link><br />
-      <Link href="week-6">Week 6</Link><br />
+      <h1 className="text-2xl font-bold m-2">CPRG 306: Web Development 2 - Assignments</h1>
+      <div className="flex flex-col m-3 ml-5">
+        <Link href="week-2">Week 2</Link>
+        <Link href="week-3">Week 3</Link>
+        <Link href="week-4">Week 4</Link>
+        <Link href="week-5">Week 5</Link>
+        <Link href="week-6">Week 6</Link>
+        <Link href="week-7">Week 7</Link>
+      </div>
     </main>
   );
 }
