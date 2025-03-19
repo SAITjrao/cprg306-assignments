b80c158ccc37fb3c969c203863054582666fb7fd
SAITjrao <jazib.rao@edu.sait.ca>
2025-03-18 22:02:47 -0600

    add meal-ideas functionality

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,8 +1,19 @@
 export default function Item(props){
+
+    const {name, quantity, category, onSelect} = props;
+
+    const handleClick = () => {
+        if (onSelect) {
+            onSelect(name);
+        }
+    };
+
     return(
-        <li className="ml-2 mb-3 p-2 bg-indigo-900 bg-opacity-30 w-96">
-            <p className="text-lg font-semibold">{props.name}</p>
-            Buy {props.quantity} in {props.category}
+        <li className="ml-2 mb-3 p-2 bg-indigo-900 bg-opacity-30 w-96 hover: bg-opacity:50"
+            onClick={handleClick}>
+
+            <p className="text-lg font-semibold">{name}</p>
+            Buy {quantity} in {category}
         </li>
-    )
+    );
 }
